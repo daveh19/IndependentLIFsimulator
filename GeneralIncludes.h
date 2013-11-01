cf9017e10819139796c4e14eddf4a783ba3a5b8d
David Higgins <dave@uiginn.com>
2013-11-01 17:23:38 -0500

    Version used for individual sims of each V_reset and each frequncy for paper.

--- GeneralIncludes.h
+++ GeneralIncludes.h
@@ -17,16 +17,16 @@
 #define RECORDER_SYNAPSE_ID (1) /*(100)*/ /*(201)*/ /* for multiple synaptic recordings this needs to be less than 400*/
 //#define RECORDER_MULTI_SYNAPSE_SKIP (450) /*(64000)*/ /*(450)*/
 
-#define USE_GPU (1) /* 1=gpu, 0=cpu */
+#define USE_GPU (0) /* 1=gpu, 0=cpu */
 #define NETWORK_SEED (-14)
 #define PARALLEL_SEED (2) /*keep positive for random123*/
 #define GAUSSIAN_SYNAPTIC_SEED (-12)
 #define UNIFORM_SYNAPTIC_SEED (-10)
 
-#define MAX_TIME_STEPS (300000) /*(12000000)*/ /*(12000000)*/ /*(300000)*/ /*no of timesteps, each of size dt*/
+#define MAX_TIME_STEPS (100000000) /*(12000000)*/ /*(12000000)*/ /*(300000)*/ /*no of timesteps, each of size dt*/
 
 // Network schema
-#define NO_EXC (200) /*(10000)*/ /*(8000)*/ /*(400)*/ /*(10000)*/
+#define NO_EXC (8000) /*(10000)*/ /*(8000)*/ /*(400)*/ /*(10000)*/
 //#define NO_INH (0)
 //#define NO_LIFS (NO_EXC + NO_INH) 
 //#define CONNECTIVITY_PROBABILITY (0.05) /*(0.05)*/
@@ -48,7 +48,7 @@
 #define J_II (-0.4)
 #define J_EI (-0.4)
 
-#define J_EXT (11.046) /*(10.23)*/ /*(10.186)*/ /*(6.215)*/ /*(8.9065)*/ /*(7.07)*/ /*(6.966) in-vivo*/ /*(7.07) 1hz in-vitro*/
+#define J_EXT (10.186) /*(11.046)*/ /*(10.23)*/ /*(10.186)*/ /*(6.215)*/ /*(8.9065)*/ /*(7.07)*/ /*(6.966) in-vivo*/ /*(7.07) 1hz in-vitro*/
 
 
 // LIF specific
@@ -62,7 +62,7 @@
 #define LIF_REFRAC_TIME (0) /*200*/ /*timesteps*/
 
 // Synapse model specific
-#define SYN_RHO_INITIAL (0.164844) /*(0.164844)*/ /*(0.16492)*/ /*(0.203586)*/ /*(1.0)*/
+#define SYN_RHO_INITIAL (0.164840) /*(0.164844)*/ /*(0.16492)*/ /*(0.203586)*/ /*(1.0)*/
 #define SYN_CA_INITIAL (0.0)
 #define SYN_CALCIUM_DELAY (461) /*46*/ /*4.6098ms*/ /*timesteps (needs to be modified when DT is modified above*/
 #define SYN_GAMMA_P (725.085)
